42bd040f754e0a5bd0268094d7b611891a5e252e
Brandon Downs <brandonmdowns@gmail.com>
2018-11-06 18:10:31 -0500

    Initial config

---
+++ SHello.pp
@@ -0,0 +1,39 @@
+# Base windows profile
+include chocolatey
+  package { "7zip.install":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
+
+  package { "notepadplusplus.install":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
+
+  package { "dotnetcore-sdk":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
+  package { "VisualStudio2017Community":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
+  package { "7zip":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
+  package { "git":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
+  package { "googlechrome":
+    ensure          => installed,
+    provider        => 'chocolatey',
+    install_options => [''],
+  }
